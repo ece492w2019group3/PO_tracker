4a52a60cc2ea78f9d4de124dcd066ebbd8074f29
Me <rcvoon@ualberta.ca>
2019-02-08 16:42:12 -0700

    Fixed for real

--- MATLAB/testSensorFusion.m
+++ MATLAB/testSensorFusion.m
@@ -4,7 +4,6 @@
 % Description: This code does some basic simulations on sample IMU data and
 % graphs the orientation along 3 axises as the IMU is lifted from a
 % surface.
-
 data = fopen('FastPickup1.txt','r'); %get the IMU data
 
 % Convert the data from the file into a matrix.
@@ -13,24 +12,29 @@ sizeA = [12 Inf];
 A = fscanf(data,formatSpec,sizeA)
 A = A'
 disp(A);
-
 % Get the acclerometer, gyroscope, and magnetometer readings from the
 % matrix.
-accelerometerReadings = A(:,5:7);
-gyroscopeReadings = A(:,2:4);
-magnetometerReadings = A(:,8:10);
-disp(accelerometerReadings);
-
+accelerometerReadings = 2.93*9.81*A(:,5:7)/1000;
+gyroscopeReadings = 0.98*A(:,2:4)*0.0174533;
+magnetometerReadings = 0.0488*A(:,8:10);
+viewer = fusiondemo.OrientationViewer;
+ifilt = imufilter;
+for ii = 1:size(accelerometerReadings, 1)
+    qimu = ifilt(accelerometerReadings(ii,:), gyroscopeReadings(ii, :));
+    viewer(qimu);
+    pause(0.01);
+end
 % Create the ahrsfilter object and fuse it with each reading from the IMU.
 decim = 2;
-fuse = ahrsfilter('SampleRate',data,'DecimationFactor',decim);
+fuse = ahrsfilter;
 q = fuse(accelerometerReadings,gyroscopeReadings,magnetometerReadings);
-
 % Plot the changes in orientation.
-time = (0:decim:size(accelerometerReadings,1)-1)/data;
-
+time = (0:size(accelerometerReadings,1)-1);
+subplot(2,1,1);
 plot(time,eulerd(q,'ZYX','frame'))
 title('Orientation Estimate')
 legend('z-axis', 'y-axis', 'x-axis')
 ylabel('Rotation (degrees)')
 fclose(data);
+
+
